b7f49cb9f75360c8b96467c099ccfda98c6ee8fa
Dayane Ramos <dayanecode@gmail.com>
2022-07-14 18:01:30 -0300

    Deploy Heroku com Postgre

--- src/main/java/com/petcare/model/PetCare.java
+++ src/main/java/com/petcare/model/PetCare.java
@@ -2,13 +2,16 @@ package com.petcare.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class PetCare {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.IDENTITY) // FUNCIONA COM O MYSQL
+	@GeneratedValue(generator = "increment") //Característica de atribuição de ID do Postgre
+	@GenericGenerator(name="increment", strategy ="increment") //Característica de atribuição de ID do Postgre
 	private Long id;
 	private String solicitante;
 	private String soliContato;
