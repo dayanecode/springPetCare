6c097d47897a8880e964f38c8338347ab3102787
Dayane Ramos <dayanecode@gmail.com>
2022-07-18 16:38:52 -0300

    incluido formulario de cadastro

--- src/main/java/com/petcare/controller/PetCareController.java
+++ src/main/java/com/petcare/controller/PetCareController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.petcare.model.PetCare;
@@ -34,5 +35,15 @@ public class PetCareController {
 		return "listarDados";
 	}
 	
+	@GetMapping("/formulario")
+	public String formulario() {
+			return "formulario";
+	}
+	
+	@PostMapping("/formularioNovo")
+	public String formularioNovo(PetCare requisicao) {
+		petcareRepository.save(requisicao);
+		return "redirect:/listarDados";
+	}
 
 }
